df1a04176da42599c4f5eca64dcc5592cc58a8a0
Nikhil <nikhil.wadatkar4693@gmail.com>
2022-07-01 00:13:34 +0530

    added

--- src/com/nt/DemoTest.java
+++ src/com/nt/DemoTest.java
@@ -1,18 +1,27 @@
 package com.nt;
 
+import java.util.function.Predicate;
+
 public class DemoTest implements GetInformation {
 
 	public static void main(String[] args) {
-		DemoTest test = new DemoTest();
 
-		System.out.println(test.showName());
-		System.out.println(test.showSurname());
-		System.out.println(GetInformation.showCity());
-		GetInformation obj = () -> {
-			return "hii";
-		};
+		Employee emp = new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0);
+//		check if Employee has salary greater than 2000
+		Predicate<Employee> p1 = (emp1) -> emp1.getSalary() > 1;
+		System.out.println(p1.test(emp));
 
-		System.out.println(obj.showName());
+//		check if Employee is male or female
+		Predicate<Employee> p2 = (emp1) -> emp1.getGender().equalsIgnoreCase("Female");
+		System.out.println(p2.test(emp));
+//		checking two conditions
+		Predicate<Employee> and2 = p1.and(p2);
+		System.out.println(and2.test(emp));
+
+		Predicate<Employee> emp2= employee-> employee.getAge()>32;
+		
+		Predicate<Employee> or = emp2.or(p2);
+		System.out.println(or.test(emp));
 	}
 
 	@Override
