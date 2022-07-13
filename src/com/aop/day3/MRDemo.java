af30ab8fb4522cfa9f83b83023dff725a17cf1ee
Ankur <ankur.wadatkar@gmail.com>
2022-07-13 20:33:51 +0530

    added java classes

---
+++ src/com/aop/day3/MRDemo.java
@@ -0,0 +1,27 @@
+package com.aop.day3;
+
+public class MRDemo {
+
+	public static void main(String[] args) {
+		// implement interface using lambda expression
+		PersonInfo personInfo = (name) -> name.toUpperCase();
+//		String nameInuppercase = personInfo.getNameInuppercase("ankur");
+		System.out.println("Name: " + nameInuppercase);
+
+		// MR to non-static: object::instance_method_name
+
+		// creating object of PersonDetails
+		PersonDetails obj = new PersonDetails();
+		// sending reference to instance method of class
+//		PersonInfo personInfo = obj::getNameInBlockLetters;
+
+		String nameInUppercase = personInfo.getNameInUppercase("nana");
+		System.out.println("Name: " + nameInUppercase);
+
+		// MR to static method : ClassName::static_Method_name;
+		InfoDetails infoDetails = PersonDetails::getLengthOfString;
+		int nameLength = infoDetails.getNameLength("Shivprasad");
+		System.out.println("length: "+nameLength);
+	}
+
+}
--- src/com/nt/CollectorsDemo.java
+++ src/com/nt/CollectorsDemo.java
@@ -1,7 +1,10 @@
 package com.nt;
 
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -11,17 +14,60 @@ public class CollectorsDemo {
 
 	public static void main(String[] args) {
 		Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6);
-		String[] cities = new String[] { "Pune", "Banglore", "Hyderabad" ,"Hyderabad"};
-		List<String> city= Stream.of(cities).collect(Collectors.toList());
-		
-		Set<String> uniqueCity=Stream.of(cities).collect(Collectors.toSet());
-		uniqueCity.forEach(System.out::println);
-		
-		Map<Integer,String> map=Stream.of(cities).collect(Collectors.toMap(String::length, String::toString));
-		Map<Object, Object> squares=Stream.of(numbers).map(num-> num).collect(Collectors.toMap(num->num, (num)->num*num));
-		
-		
-		
+		String[] cities = new String[] { "Pune", "Banglore", "Hyderabad", "Lahore" };
+		List<String> city = Stream.of(cities).collect(Collectors.toList());
+
+		// collect method list
+		List<String> upperCities = Stream.of(cities).collect(Collectors.toList());
+		System.out.println("Upper cities" + upperCities);
+		// collect method set
+		Set<String> uniqueCities = Stream.of(cities).collect(Collectors.toSet());
+		System.out.println("Unique cities:" + uniqueCities);
+
+		// collect method map
+		Map<Integer, String> mapCities = Stream.of(cities).collect(Collectors.toMap(String::length, str -> str));
+		System.out.println("Map cities" + mapCities);
+
+		String[] strings = Stream.of(cities).toArray(String[]::new);
+		for (String string : strings) {
+			System.out.println(string);
+		}
+		System.out.println();
+
+		// joining
+		String allCities = Stream.of(cities).collect(Collectors.joining(" , "));
+		System.out.println("allCities:" + allCities);
+
+		// counting
+		Long noOfCities = Stream.of(cities).collect(Collectors.counting());
+		System.out.println("NoOf Cities:" + noOfCities);
+
+		// max and min
+		Optional<String> maxCityLength = Stream.of(cities)
+				.collect(Collectors.maxBy(Comparator.comparing(String::length)));
+		System.out.println("Max city length: " + maxCityLength.get());
+
+		Optional<String> minCity = Stream.of(cities).collect(Collectors.minBy(Comparator.comparing(String::toString)));
+		System.out.println("Min city: " + minCity.get());
+
+		Integer sumOfNumbers = numbers.collect(Collectors.summingInt(Integer::valueOf));
+		System.out.println("Sum :" + sumOfNumbers);
+
+		// summerizing => count,max,min,average,sum
+		IntSummaryStatistics sum = numbers.collect(Collectors.summarizingInt(Integer::intValue));
+		System.out.println(sum.getAverage());
+		System.out.println(sum.getCount());
+		System.out.println(sum.getMax());
+		System.out.println(sum.getMin());
+		System.out.println(sum.getSum());
 	}
 
 }
+
+//Collecting the stream elements (which are filtered/processed) into various kinds for formats like collection or array
+//Joining or counting
+//Finding max or min
+//Doing sum of any thing
+//Grouping or portioning 
+//Finding average for any int, long, double kind of property
+//Summarizing for any int, long, double kind of property
